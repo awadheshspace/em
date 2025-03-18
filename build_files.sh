762758c02f79ea1cd672afb7817ea9572f495495
exammantra <demonstrate1106@gmail.com>
2025-03-18 22:48:37 +0530

    add pachage

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-set -x
-chmod +x build_files.sh
+#!/bin/bash
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# Install Python and pip (if necessary)
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Ensure pip is up-to-date
+python3 -m pip install --upgrade pip
+
+# Install project dependencies
+pip3 install -r requirements.txt
+
+# Any other build steps you need, such as collecting static files or running migrations
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
