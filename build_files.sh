fbe5bbdae14a5eb7111413c2c8b51779c74875f7
exammantra <demonstrate1106@gmail.com>
2025-03-18 22:56:17 +0530

    add pachage

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Install Python and pip (if necessary)
+# Update package lists and install Python 3 and pip3
 apt-get update
 apt-get install -y python3 python3-pip
 
-# Ensure pip is up-to-date
+# Ensure pip3 is up-to-date
 python3 -m pip install --upgrade pip
 
-# Install project dependencies
+# Install the dependencies from requirements.txt
 pip3 install -r requirements.txt
 
-# Any other build steps you need, such as collecting static files or running migrations
+# Collect static files and run migrations (if necessary)
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
