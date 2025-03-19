65a0f8d5239e1a496c0ed5cc1e07e41985074d36
exammantra <demonstrate1106@gmail.com>
2025-03-19 11:38:38 +0530

    add some

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,9 @@
-echo "BUILD STARTED"
 
-pip3 install -r requirements.txt
+
+pip install -r requirements.txt
 
 # Collect static files (if you're using Django's static files)
 python3 manage.py collectstatic --noinput
 
 # Run database migrations (if necessary)
 python3 manage.py migrate
-echo "BUILD COMPLETED"
