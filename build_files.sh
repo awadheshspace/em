dbcc460d6c9ba8f0be8419b4e08f55a6e3371d5a
exammantra <demonstrate1106@gmail.com>
2025-03-19 11:54:23 +0530

    add some package

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 pip install -r requirements.txt
 
 # Collect static files (if you're using Django's static files)
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Run database migrations (if necessary)
-python3 manage.py migrate
+python manage.py migrate
